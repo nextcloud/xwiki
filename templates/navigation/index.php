cda3ebc9ec618556f9872667947251e5669a88a0
Raphaël Jakse <raphael.jakse@xwiki.com>
2023-05-15 11:21:06 +0200

    #10 Fix navigation tree not being shown when only one instance is registered

--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -1,34 +1,30 @@
 <?php if ($_['integratedMode']) { ?>
 <ul id="xwiki-nav"><?php
-if (count($_['instances']) > 1) {?>
-	<?php
-		foreach ($_['instances'] as $instance) {?>
-			<li>
-				<a href="<?php p($_['urlGenerator']->linkToRoute('xwiki.page.index', [
-					'i' => $instance->url,
-					'p' => 'xwiki:Main',
-				]));?>">
-					<span class="icon icon-xwiki"></span>
-					<?php p($instance->getPrettyName());?>
-				</a><?php
-				if ($_['currentInstance']->url === $instance->url && !empty($_['pages'])) {?>
-					<ul><?php
-						foreach ($_['pages'] as $page) {?>
-							<li>
-								<a href="<?php p($page['url']); ?>">
-									<span class="icon icon-xwiki-page"></span>
-									<?php
-										p($page['title']);
-									?>
-								</a>
-							</li><?php
-						}
-					?>
-					</ul><?php
-				} ?>
-			</li><?php
-		} ?>
-	<?php
-} ?>
+	foreach ($_['instances'] as $instance) {?>
+		<li>
+			<a href="<?php p($_['urlGenerator']->linkToRoute('xwiki.page.index', [
+				'i' => $instance->url,
+				'p' => 'xwiki:Main',
+			]));?>">
+				<span class="icon icon-xwiki"></span>
+				<?php p($instance->getPrettyName());?>
+			</a><?php
+			if ($_['currentInstance']->url === $instance->url && !empty($_['pages'])) {?>
+				<ul><?php
+					foreach ($_['pages'] as $page) {?>
+						<li>
+							<a href="<?php p($page['url']); ?>">
+								<span class="icon icon-xwiki-page"></span>
+								<?php
+									p($page['title']);
+								?>
+							</a>
+						</li><?php
+					}
+				?>
+				</ul><?php
+			} ?>
+		</li><?php
+	} ?>
 </ul>
 <?php } ?>
